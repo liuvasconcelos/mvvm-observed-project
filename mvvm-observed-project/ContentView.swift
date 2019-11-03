917e75e3f5e636d93cc7b197b122a9ea1179e0fc
liuvasconcelos <livia.vasconcelos@stant.com.br>
2019-11-03 18:20:46 -0300

    develop Add fetching objects.
    -
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

--- mvvm-observed-project/ContentView.swift
+++ mvvm-observed-project/ContentView.swift
@@ -10,14 +10,42 @@ import SwiftUI
 
 let apiUrl = "https://api.letsbuildthatapp.com/static/courses.json"
  
+struct Course: Identifiable, Decodable {
+    let id =  UUID()
+    let name: String
+}
+
+class CoursesViewModel: ObservableObject {
+    @Published var messages = "Messages inside observable object"
+    @Published var courses: [Course] = []
+    
+    func fetchCourses() {
+        guard let url = URL(string: apiUrl) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                self.courses = try! JSONDecoder().decode([Course].self, from: data!)
+            }
+        }.resume()
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var coursesViewModel = CoursesViewModel()
+    
     var body: some View {
         NavigationView {
             ScrollView {
-                Text("Hello")
+                Text(coursesViewModel.messages)
+                
+                ForEach(coursesViewModel.courses) { course in
+                    Text(course.name)
+                }
             }.navigationBarTitle("Courses")
                 .navigationBarItems(trailing: Button(action: {
                     print("Fetching json")
+                    
+                    self.coursesViewModel.fetchCourses()
                 }, label: {
                     Text("Fetch Courses")
                 }))
