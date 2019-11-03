093955d33360b553a39d4aed6ef05a61057830b7
liuvasconcelos <livia.vasconcelos@stant.com.br>
2019-11-03 17:56:23 -0300

    Initial Commit

---
+++ mvvm-observed-project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mvvm-observed-project
+//
+//  Created by Livia Vasconcelos on 03/11/19.
+//  Copyright © 2019 Livia Vasconcelos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mvvm-observed-projectTests/mvvm_observed_projectTests.swift
@@ -0,0 +1,34 @@
+//
+//  mvvm_observed_projectTests.swift
+//  mvvm-observed-projectTests
+//
+//  Created by Livia Vasconcelos on 03/11/19.
+//  Copyright © 2019 Livia Vasconcelos. All rights reserved.
+//
+
+import XCTest
+@testable import mvvm_observed_project
+
+class mvvm_observed_projectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
